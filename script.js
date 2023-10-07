c5c5038277b4123dc5e15e3462a0f20b075de493
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-10-07 12:50:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const byteSize = (str) => {
-  // write your code here
-};
+function byteSize(str) {
+  // Create a Blob object with the input string
+  const blob = new Blob([str]);
 
-// Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+  // Get the size of the Blob in bytes
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
+}
+
+// Test cases
+console.log(byteSize('hello world'));    // Output: 11
+console.log(byteSize('안녕하세요'));     // Output: 15
+console.log(byteSize(''));              // Output: 0
